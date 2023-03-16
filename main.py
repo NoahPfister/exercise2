a3de2fd6dd18a60a503895af073310001a97331d
NoahPfister <112487984+NoahPfister@users.noreply.github.com>
2023-03-16 15:42:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-import arcade
+#ici j'importe
+import arcade 
 import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+#ici je met les couleurs
 COLORS = [arcade.color.BLUE, arcade.color.FANDANGO_PINK,
         arcade.color.FRENCH_ROSE, arcade.color.GOLDEN_POPPY]
-
+#je cree le cercle
 class Cercle():
   def __init__(self, r,x,y,c):
       self.rayon = r
       self.centre_x = x
       self.centre_y = y
       self.color = (c)
-
+#je decine le cercle
   def draw(self):
       #arcade.draw_circle_filled(center_x, center_y, rayon, color)
     arcade.draw_circle_filled(self.centre_x, self.centre_y, self.rayon, self.color)
 
-
+#affichez le jeux
 class MyGame(arcade.Window):
    def __init__(self):
       super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
       self.liste_cercles = []
 
 
-
+#definir cercle
    def setup(self):
       # remplir la liste avec 20 objets de type Cercle
       for _ in range(20):
@@ -35,13 +37,13 @@ class MyGame(arcade.Window):
           color = random.choice(COLORS)
           cercle= Cercle(rayon, center_x, center_y, color)
           self.liste_cercles.append(cercle)
-
+(commenceer le render)
    def on_draw(self):
       arcade.start_render()
 
       for cercle in self.liste_cercles:
           cercle.draw()
-
+(faire en sorte de effacer lescercle
    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
        if button == 1:
            for i in self.liste_cercles:
@@ -52,6 +54,8 @@ class MyGame(arcade.Window):
 
                if maxpositionX > x > minpositionX and maxpositiponY > y >minpositionY:
                    self.liste_cercles.remove(i)
+ 
+ #randomiser les couleurs
        elif button == 4:
                for i in self.liste_cercles:
                    maxpositionX = i.centre_x + i.rayon
